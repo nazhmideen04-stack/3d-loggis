cf89095359081511c9eba3210347d82c3baa6ad4
nazhmideen04-stack <nazhmideen.04@gmail.com>
2026-09-22 13:44:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 import streamlit as st
 from playwright.sync_api import sync_playwright
 
-st.set_page_config(page_title="LOGGIS 3B Tünel İzleme", layout="wide")
+st.set_page_config(page_title="LOGGIS 3B", layout="wide")
 
 URL = "https://loggis2.com/?company-id=20ce6d9f-398b-43b3-a452-3580dae39122&project-id=2d381d12-d966-4c90-a7c8-c90d6f758ae0&token-id=6e73d15f-0b2f-4d93-a152-3464f7450e50"
 
